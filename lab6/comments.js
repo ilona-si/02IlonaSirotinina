c520b9ac2652a682ed5fa700dbcd4206d57dbc84
ilona sirotinina <ilona sirotinina>
2016-10-24 02:47:44 +0300

    lab 6 a first part

---
+++ lab6/comments.js
@@ -0,0 +1,26 @@
+var names =['Ilona','Denis','Kate','Nastya','Sergei','Vlad'];
+var cities = ['Минска', 'Гомеля', 'Добруша','Москвы','Киева']; 
+var goods = [' Стул АС18', 'Мультиварка Lenovo xPro', 'Смартфон Nokia 3320','Пылесос Samsung', 'Утюг Bosh'];
+
+var list = document.getElementById('div');
+setInterval(createComment, 3000);
+
+function createComment() {
+	var input = document.createElement('input');
+	input.type = 'button';
+	input.value = 'close';
+	var div = document.createElement('div');
+	var name = names[Math.floor(Math.random()*names.length)];
+	var city = cities[Math.floor(Math.random()*cities.length)];
+	var item = goods[Math.floor(Math.random()*goods.length)];
+	var i = Math.floor(Math.random()*11);
+	var comment = document.createTextNode(name +'  из  '+ city+ '  купил(а)  '+ item+ ' ' +i+'шт товара');
+	div.appendChild(comment);
+	div.appendChild(input);
+	list.appendChild(div);
+
+	input.addEventListener('click', function(){
+		div.parentNode.removeChild(div);
+	});
+}
+
